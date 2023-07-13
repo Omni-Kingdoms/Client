45d8d81511fabf3716c0bf81d41764830cddff86
Rafael Ovidio Leal <rafael.leal@stage.consulting>
2023-07-13 19:23:46 -0300

    Update Character.tsx

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -259,14 +259,17 @@ export default function Character() {
               Create Character
             </button>
           </form>
+
+          {isLoading && (
+            <div>
+              <span className="relative inset-0 inline-flex h-6 w-6 animate-spin items-center justify-center rounded-full border-2 border-gray-300 after:absolute after:h-8 after:w-8 after:rounded-full after:border-2 after:border-y-indigo-500 after:border-x-transparent"></span>
+            </div>
+          )}
         </div>
+
       </div>
 
-      {isLoading && (
-        <div>
-          <span className="relative inset-0 inline-flex h-6 w-6 animate-spin items-center justify-center rounded-full border-2 border-gray-300 after:absolute after:h-8 after:w-8 after:rounded-full after:border-2 after:border-y-indigo-500 after:border-x-transparent"></span>
-        </div>
-      )}
+      
     </>
   );
 }
