a256abab03838bb8a27943441bcc577e4deb83a3
Rafael Ovidio Leal <rafael.leal@stage.consulting>
2023-07-14 15:52:57 -0300

    feat: Ajuste Loading

--- src/components/Character/Character.tsx
+++ src/components/Character/Character.tsx
@@ -291,9 +291,9 @@ export default function Character() {
             </button>
           </form>
           {isLoading && (
-              <div>
-                <span className="relative inset-0 inline-flex h-6 w-6 animate-spin items-center justify-center rounded-full border-2 border-gray-300 after:absolute after:h-8 after:w-8 after:rounded-full after:border-2 after:border-y-indigo-500 after:border-x-transparent"></span>
-              </div>
+            <div className="min-[1023px]:relative min-[1023px]:right-28">
+              <span className="relative inset-0 inline-flex h-6 w-6 animate-spin items-center justify-center rounded-full border-2 border-gray-300 after:absolute after:h-8 after:w-8 after:rounded-full after:border-2 after:border-y-indigo-500 after:border-x-transparent"></span>
+            </div>
           )}
         </div>
       </div>
