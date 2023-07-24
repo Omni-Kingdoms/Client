f7d6bf824b7becd483799add65bb7cd7fd97963a
Alexandre Carvalheira <axxcar@gmail.com>
2023-07-24 15:13:43 -0300

    render button

--- src/components/PlayerCard/index.tsx
+++ src/components/PlayerCard/index.tsx
@@ -8,10 +8,10 @@ import React, { useState, useRef, useEffect } from "react";
 import molde from "@/assets/img/components/PlayerCard/molde.png";
 import paper from "@/assets/img/components/PlayerCard/paper.png";
 
-import arrowLeft from "@/assets/img/components/PlayerCard/icons/arrow-left.svg"
-import arrowRight from "@/assets/img/components/PlayerCard/icons/arrow-right.svg"
-import arrowLeftDisable from "@/assets/img/components/PlayerCard/icons/arrow-left-disable.svg"
-import arrowRightDisable from "@/assets/img/components/PlayerCard/icons/arrow-right-disable.svg"
+import arrowLeft from "@/assets/img/components/PlayerCard/icons/arrow-left.svg";
+import arrowRight from "@/assets/img/components/PlayerCard/icons/arrow-right.svg";
+import arrowLeftDisable from "@/assets/img/components/PlayerCard/icons/arrow-left-disable.svg";
+import arrowRightDisable from "@/assets/img/components/PlayerCard/icons/arrow-right-disable.svg";
 
 import Mage1 from "@/assets/img/personas/playerCard/Mage-1.png";
 import Mage0 from "@/assets/img/personas/playerCard/Mage-0.png";
@@ -33,40 +33,9 @@ export const Player = () => {
     <Image src={Mage1} alt="chest" className="relative w-44 -left-2.5 -top-7" />
   );
 
-<<<<<<< HEAD
-  const handleRightClick = (
-    e: React.MouseEvent<HTMLImageElement, MouseEvent>
-  ) => {
-    const target = e.target as HTMLImageElement;
-    var elementRemove = document.getElementById("arrowLeft");
-    elementRemove?.classList.remove("gray-img");
-
-    if (currentPlayerIndex == players.length - 1) {
-      var elementAdd = document.getElementById(target.id);
-      elementAdd?.classList.add("gray-img");
-    }
-
-    if (currentPlayerIndex < players.length - 1) {
-      setCurrentPlayerIndex(currentPlayerIndex + 1);
-    }
-  };
-
-  const handleLeftClick = (
-    e: React.MouseEvent<HTMLImageElement, MouseEvent>
-  ) => {
-    const target = e.target as HTMLImageElement;
-    var elementRemove = document.getElementById("arrowRight");
-    elementRemove?.classList.remove("gray-img");
-
-    if (currentPlayerIndex == 0) {
-      var elementAdd = document.getElementById(target.id);
-      elementAdd?.classList.add("gray-img");
-    }
-
-    if (currentPlayerIndex != 0) {
-      setCurrentPlayerIndex(currentPlayerIndex - 1);
-    }
-  };
+  const [index, setIndex] = useState(currentPlayerIndex);
+  setCurrentPlayerIndex(index);
+  localStorage.setItem("PlayerIndex", index.toString());
 
   const currentPlayer = playerStore((state) => state.currentPlayer);
   let currentClass = "";
@@ -120,48 +89,19 @@ export const Player = () => {
       />
     );
   }
-=======
-  const [index, setIndex] = useState(currentPlayerIndex);
-  setCurrentPlayerIndex(index);
-  localStorage.setItem("PlayerIndex", index.toString());
->>>>>>> stage2
 
-  const currentPlayer = playerStore((state) => state.currentPlayer);
-  let currentClass = "";
-
-  if(currentPlayer?.playerClass == 0 && currentPlayer?.male){
-    setImage = (<Image src={Knight1} alt="Knight1" className="relative w-44 -left-2.5 -top-7"/>);
-  } else if(currentPlayer?.playerClass == 0 && !currentPlayer?.male){
-    setImage = (<Image src={Knight0} alt="Knight0" className="relative w-44 -left-2.5 -top-7"/>);
-  } else if(currentPlayer?.playerClass == 1 && currentPlayer?.male){
-    setImage = (<Image src={Assassin1} alt="Assassin1" className="relative w-44 -left-2.5 -top-7"/>);
-  } else if(currentPlayer?.playerClass == 1 && !currentPlayer?.male){
-    setImage = (<Image src={Assassin0} alt="Assassin0" className="relative w-44 -left-2.5 -top-7"/>);
-  } else if(currentPlayer?.playerClass == 2 && currentPlayer?.male){
-    setImage = (<Image src={Mage1} alt="Mage1" className="relative w-44 -left-2.5 -top-7"/>);
-  }else {
-    setImage = (<Image src={Mage0} alt="Mage0" className="relative w-44 -left-2.5 -top-7"/>);
-  }
-
-  if(currentPlayer?.playerClass == 0){
+  if (currentPlayer?.playerClass == 0) {
     currentClass = "Knight";
-  } else if(currentPlayer?.playerClass == 1){
-    currentClass ="Assassin";
+  } else if (currentPlayer?.playerClass == 1) {
+    currentClass = "Assassin";
   } else {
     currentClass = "Mage";
   }
 
   return (
-<<<<<<< HEAD
     <>
-      <div className="relative left-16 top-10">
-=======
-   <>
       <div className="relative left-16 top-2">
->>>>>>> stage2
         <div className="absolute max-h-8">
-          {setImage}
-          <div className="top-1 absolute w-38 text-center pb-8 stats">
           {setImage}
           <div className="top-1 absolute w-38 text-center pb-8 stats">
             <p>{currentClass}</p>
@@ -175,63 +115,64 @@ export const Player = () => {
             />
             <div className="absolute name top-1.5 w-48 text-center">
               <p>{currentPlayer?.name}</p>
-              <p className="relative -top-1.5 text-xs">#{Number(players[currentPlayerIndex])}</p>
+              <p className="relative -top-1.5 text-xs">
+                #{Number(players[currentPlayerIndex])}
+              </p>
             </div>
             {players.length > 1 ? (
               <>
-              <button>
-                <Image
-                  src={arrowLeft}
-                  id="arrowLeft"
-                  onClick={() =>
-                    index === 0
-                      ? setIndex(players.length - 1)
-                      : setIndex(index - 1)
-                  }
-                  className="button-left -left-33"
-                  alt="arrowLeft"
-                />
-              </button>
-              <button>
-                <Image
-                  src={arrowRight}
-                  id="arrowRight"
-                  onClick={() =>
-                    index === players.length - 1
-                      ? setIndex(0)
-                      : setIndex(index + 1)
-                  }
-                  className="button-next"
-                  alt="arrowRight"
-                />
-              </button>
+                <button>
+                  <Image
+                    src={arrowLeft}
+                    id="arrowLeft"
+                    onClick={() =>
+                      index === 0
+                        ? setIndex(players.length - 1)
+                        : setIndex(index - 1)
+                    }
+                    className="button-left -left-33"
+                    alt="arrowLeft"
+                  />
+                </button>
+                <button>
+                  <Image
+                    src={arrowRight}
+                    id="arrowRight"
+                    onClick={() =>
+                      index === players.length - 1
+                        ? setIndex(0)
+                        : setIndex(index + 1)
+                    }
+                    className="button-next"
+                    alt="arrowRight"
+                  />
+                </button>
               </>
             ) : (
               <>
-              <button disabled>
-                <Image
-                  src={arrowLeftDisable}
-                  id="arrowLeft"
-                  className="button-left -left-33"
-                  alt="arrowLeft"
-                />
-              </button>
-              <button disabled>
-                <Image
-                  src={arrowRightDisable}
-                  id="arrowRight"
-                  onClick={() =>
-                    index === players.length - 1
-                      ? setIndex(0)
-                      : setIndex(index + 1)
-                  }
-                  className="button-next"
-                  alt="arrowRight"
-                />
-              </button>
+                <button disabled>
+                  <Image
+                    src={arrowLeftDisable}
+                    id="arrowLeft"
+                    className="button-left -left-33"
+                    alt="arrowLeft"
+                  />
+                </button>
+                <button disabled>
+                  <Image
+                    src={arrowRightDisable}
+                    id="arrowRight"
+                    onClick={() =>
+                      index === players.length - 1
+                        ? setIndex(0)
+                        : setIndex(index + 1)
+                    }
+                    className="button-next"
+                    alt="arrowRight"
+                  />
+                </button>
               </>
             )}
-            
           </div>
         </div>
         <PlayerBars />
@@ -240,4 +181,3 @@ export const Player = () => {
     </>
   );
 };
-
