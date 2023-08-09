e152c400b906fc35f2e358628fcacc30c9ec420a
caiolima-stage <caio.lima@stage.consulting>
2023-08-09 14:49:48 -0300

    feat: equipment and consumables icon on shop

--- src/app/play/shop/page.tsx
+++ src/app/play/shop/page.tsx
@@ -3,6 +3,8 @@ import map from "@/assets/img/components/Shop/map.png";
 import Image from 'next/image';
 
 import shop from "@/assets/img/components/Play/shop.png"
+import consumables from "@/assets/img/components/Shop/consumables_shop.png";
+import equipments from "@/assets/img/components/Shop/equipments_shop.png";
 import { Tooltip } from 'antd';
 
 export default function Shop() {
@@ -12,10 +14,19 @@ export default function Shop() {
         <Image src={map} alt="Mapa" />
       </div>
       <div className="icon-right min-[2000px]:right-64 min-[3000px]:mr-96">
-        <Tooltip title="Shop Items">
+        <Tooltip title="Equipment">
           <button type="button">
             <Image
-              src={shop}
+              src={equipments}
+              className="icons-map hover:cursor-pointer icons-map min-[400px]:m-5"
+              alt="mapa"
+            />
+          </button>
+        </Tooltip>
+        <Tooltip title="Consumables">
+          <button type="button">
+            <Image
+              src={consumables}
               className="icons-map hover:cursor-pointer icons-map min-[400px]:m-5"
               alt="mapa"
             />
