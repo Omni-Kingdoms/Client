f23cd239053e1fce48eaa6c7bc6610c27bb4f7bd
caiolima-stage <caio.lima@stage.consulting>
2023-08-11 20:55:17 -0300

    feat: row structure added

--- src/components/Modal/Shop/EquipItem.tsx
+++ src/components/Modal/Shop/EquipItem.tsx
@@ -1,5 +1,6 @@
 import { contractStore } from '@/store/contractStore';
 import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
+import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
 
 type EquipItemProps = {
@@ -12,9 +13,13 @@ export default function EquipItem({ id, loadingCount, disableLoading }: EquipIte
   const [equip, setEquip] = useState<Equip | null>();
   const contract = contractStore((state) => state.diamond);
 
+  async function handleBuyEquip() {
+
+  }
+
   const loadEquip = useCallback(async () => {
     console.log(id);
-    const equip = await contract.read.getEquipment([id]);
+    const equip = await contract.read.getBasicEquipmentSchema([id]);
     console.log(equip);
 
     disableLoading();
@@ -25,11 +30,19 @@ export default function EquipItem({ id, loadingCount, disableLoading }: EquipIte
     loadEquip();
   }, [loadEquip])
 
-  console.log(equip);
-
   if (!equip || !equip?.name || loadingCount) return <></>;
 
   return (
-    <div className="col-span-4">{equip.name}</div>
+    <div className="col-span-full custom-list-item grid grid-cols-4 w-[100%] place-items-center p-4 rounded">
+      <div className="equip-name place-self-start self-center flex items-center gap-4">
+        <Image src={equip?.uri!} width={40} height={40} alt="Equip icon" className="rounded-full" />
+        <p>{equip.name}</p>
+      </div>
+      <div className="equip-value">{Number(equip.value)}</div>
+      <div className="equip-cost">{Number(equip.cost)}</div>
+      <div className="equip-action place-self-end self-center">
+        <button type="button" className="p-3 rounded" onClick={handleBuyEquip}>Buy</button>
+      </div>
+    </div>
   )
 }
--- src/components/Modal/Shop/PotionItem.tsx
+++ src/components/Modal/Shop/PotionItem.tsx
@@ -1,5 +1,6 @@
 import { contractStore } from '@/store/contractStore';
 import { BasicPotionStruct as Potion } from '@/types/DIAMOND1HARDHAT';
+import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
 
 type PotionItemProps = {
@@ -11,6 +12,8 @@ type PotionItemProps = {
 export default function PotionItem({ id, loadingCount, disableLoading }: PotionItemProps) {
   const [potion, setPotion] = useState<Potion | null>();
   const contract = contractStore((state) => state.diamond);
+
+  async function handleBuyPotion() {}
 
   const loadPotion = useCallback(async () => {
     const potion = await contract.read.getBasicPotion([id]);
@@ -26,6 +29,16 @@ export default function PotionItem({ id, loadingCount, disableLoading }: PotionI
   if (!potion || !potion?.name || loadingCount) return <></>;
 
   return (
-    <div className="col-span-4">{potion.name}</div>
+    <div className="col-span-full custom-list-item grid grid-cols-4 w-[100%] place-items-center p-4 rounded">
+      <div className="potion-name flex place-self-start self-center items-center gap-4">
+        <Image src={potion?.uri!} width={40} height={40} alt="Potion icon" className="rounded-full" />
+        <p>{potion.name}</p>
+      </div>
+      <div className="potion-value">{Number(potion.value)}</div>
+      <div className="potion-cost">{Number(potion.cost)}</div>
+      <div className="potion-action place-self-end self-center">
+        <button type="button" className="p-3 rounded" onClick={handleBuyPotion}>Buy</button>
+      </div>
+    </div>
   )
 }
