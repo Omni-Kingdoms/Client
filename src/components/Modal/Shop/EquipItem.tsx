3d0f8fa731c3da2e0bcc9ab1c77da55ab545b294
caiolima-stage <caio.lima@stage.consulting>
2023-08-11 21:56:04 -0300

    feat: purchase item methods implemented

--- src/components/Modal/Shop/EquipItem.tsx
+++ src/components/Modal/Shop/EquipItem.tsx
@@ -1,7 +1,10 @@
 import { contractStore } from '@/store/contractStore';
+import { playerStore } from '@/store/playerStore';
 import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { usePublicClient } from 'wagmi';
 
 type EquipItemProps = {
   id: Number | BigInt,
@@ -10,11 +13,63 @@ type EquipItemProps = {
 }
 
 export default function EquipItem({ id, loadingCount, disableLoading }: EquipItemProps) {
-  const [equip, setEquip] = useState<Equip | null>();
   const contract = contractStore((state) => state.diamond);
+  const players = playerStore((state) => state.players);
+  const currentPlayerIndex = playerStore((state) => state.currentPlayerIndex);
+  const currentPlayerGold = playerStore((state) => state.gold);
+  const setCurrentPlayer = playerStore((state) => state.setCurrentPlayer);
+  const setGold = playerStore((state) => state.setGold);
+
+  const [equip, setEquip] = useState<Equip | null>();
+
+  const publicClient = usePublicClient();
 
   async function handleBuyEquip() {
+    try {
+      const hash = await contract.write.purchaseBasicEquipment([
+        players[currentPlayerIndex!],
+        id,
+      ]);
+      const loading = toast.loading("Tx pending: " + hash);
+      const result = await publicClient.waitForTransactionReceipt({
+        hash,
+      });
 
+      if (result.status === "success") {
+        toast.update(loading, {
+          render: "Success: " + hash,
+          type: "success",
+          isLoading: false,
+          autoClose: 5000,
+        });
+
+        setGold(Number(currentPlayerGold) - Number(equip?.cost));
+
+        const player = await contract.read.getPlayer([
+          players[currentPlayerIndex!],
+        ]);
+
+        setCurrentPlayer(player);
+      } else {
+        toast.update(loading, {
+          render: "Failed: " + hash,
+          type: "error",
+          isLoading: false,
+          autoClose: 5000,
+        });
+      }
+    } catch (error: any) {
+      toast.error(error.shortMessage as string, {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
   }
 
   const loadEquip = useCallback(async () => {
@@ -32,6 +87,8 @@ export default function EquipItem({ id, loadingCount, disableLoading }: EquipIte
 
   if (!equip || !equip?.name || loadingCount) return <></>;
 
+  const playerCanBuyItem = currentPlayerGold >= Number(equip.cost);
+
   return (
     <div className="col-span-full custom-list-item grid grid-cols-4 w-[100%] place-items-center p-4 rounded">
       <div className="equip-name place-self-start self-center flex items-center gap-4">
@@ -41,7 +98,12 @@ export default function EquipItem({ id, loadingCount, disableLoading }: EquipIte
       <div className="equip-value">{Number(equip.value)}</div>
       <div className="equip-cost">{Number(equip.cost)}</div>
       <div className="equip-action place-self-end self-center">
-        <button type="button" className="p-3 rounded" onClick={handleBuyEquip}>Buy</button>
+        <button
+          type="button"
+          className="p-3 rounded bg-button"
+          onClick={handleBuyEquip}
+          disabled={!playerCanBuyItem}
+        >Buy</button>
       </div>
     </div>
   )
--- src/components/Modal/Shop/PotionItem.tsx
+++ src/components/Modal/Shop/PotionItem.tsx
@@ -1,7 +1,10 @@
 import { contractStore } from '@/store/contractStore';
+import { playerStore } from '@/store/playerStore';
 import { BasicPotionStruct as Potion } from '@/types/DIAMOND1HARDHAT';
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import { usePublicClient } from 'wagmi';
 
 type PotionItemProps = {
   id: Number | BigInt,
@@ -10,10 +13,64 @@ type PotionItemProps = {
 }
 
 export default function PotionItem({ id, loadingCount, disableLoading }: PotionItemProps) {
-  const [potion, setPotion] = useState<Potion | null>();
   const contract = contractStore((state) => state.diamond);
+  const players = playerStore((state) => state.players);
+  const currentPlayerIndex = playerStore((state) => state.currentPlayerIndex);
+  const currentPlayerGold = playerStore((state) => state.gold);
+  const setCurrentPlayer = playerStore((state) => state.setCurrentPlayer);
+  const setGold = playerStore((state) => state.setGold);
 
-  async function handleBuyPotion() {}
+  const [potion, setPotion] = useState<Potion | null>();
+
+  const publicClient = usePublicClient();
+
+  async function handleBuyPotion() {
+    try {
+      const hash = await contract.write.purchaseBasicPotion([
+        players[currentPlayerIndex!],
+        id,
+      ]);
+      const loading = toast.loading("Tx pending: " + hash);
+      const result = await publicClient.waitForTransactionReceipt({
+        hash,
+      });
+
+      if (result.status === "success") {
+        toast.update(loading, {
+          render: "Success: " + hash,
+          type: "success",
+          isLoading: false,
+          autoClose: 5000,
+        });
+
+        setGold(Number(currentPlayerGold) - Number(potion?.cost));
+
+        const player = await contract.read.getPlayer([
+          players[currentPlayerIndex!],
+        ]);
+
+        setCurrentPlayer(player);
+      } else {
+        toast.update(loading, {
+          render: "Failed: " + hash,
+          type: "error",
+          isLoading: false,
+          autoClose: 5000,
+        });
+      }
+    } catch (error: any) {
+      toast.error(error.shortMessage as string, {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    }
+  }
 
   const loadPotion = useCallback(async () => {
     const potion = await contract.read.getBasicPotion([id]);
@@ -28,6 +85,8 @@ export default function PotionItem({ id, loadingCount, disableLoading }: PotionI
 
   if (!potion || !potion?.name || loadingCount) return <></>;
 
+  const playerCanBuyItem = currentPlayerGold >= Number(potion.cost);
+
   return (
     <div className="col-span-full custom-list-item grid grid-cols-4 w-[100%] place-items-center p-4 rounded">
       <div className="potion-name flex place-self-start self-center items-center gap-4">
@@ -37,7 +96,12 @@ export default function PotionItem({ id, loadingCount, disableLoading }: PotionI
       <div className="potion-value">{Number(potion.value)}</div>
       <div className="potion-cost">{Number(potion.cost)}</div>
       <div className="potion-action place-self-end self-center">
-        <button type="button" className="p-3 rounded" onClick={handleBuyPotion}>Buy</button>
+        <button
+          type="button"
+          className="p-3 rounded bg-button"
+          onClick={handleBuyPotion}
+          disabled={!playerCanBuyItem}
+        >Buy</button>
       </div>
     </div>
   )
