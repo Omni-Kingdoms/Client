ebaac8102c37ee4642ca03793c41d327e29c8529
caiolima-stage <caio.lima@stage.consulting>
2023-08-14 18:04:42 -0300

    feat: close button feature fixed

--- src/components/Modal/Equipment/Equipment.tsx
+++ src/components/Modal/Equipment/Equipment.tsx
@@ -1,10 +1,10 @@
 import "./index.css";
 
-import { useRef } from 'react'
+import { MouseEvent, useRef } from 'react'
 import Image from 'next/image';
 import { useOnClickOutside } from 'usehooks-ts';
 
-import fechar from "@/assets/img/components/modal/X.png";
+import closeIcon from "@/assets/img/components/modal/X.png";
 import paperback1 from '@/assets/img/components/Equipment/paperback1.png';
 import paperback2 from '@/assets/img/components/Equipment/paperback2.png';
 
@@ -15,22 +15,23 @@ type EquipmentProps = {
 export default function Equipment({ close }: EquipmentProps) {
   const equipmentRef = useRef(null);
 
-  useOnClickOutside(equipmentRef, close);
+  function blockPropagation(e: MouseEvent) {
+    e.stopPropagation();
+  }
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="fixed inset-0 backdrop-blur-sm">
+      <div onClick={close} className="fixed inset-0 backdrop-blur-sm">
         <div className="relative h-[100vh]">
-          <button type="button" className="absolute top-22 right-16" onClick={close}>
-            <Image src={fechar} alt="Fechar lista" />
-          </button>
-          <div className="bg-equip z-10 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+          <div onClick={blockPropagation} className="bg-equip z-20 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
             <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible" />
             <div className="content absolute inset-0 p-24 flex flex-col">
-              {/* Content goes here */}
+            <button type="button" className="absolute top-4 right-6 z-20" onClick={close}>
+              <Image src={closeIcon} alt="closeIcon lista" />
+            </button>
             </div>
           </div>
-          <div className="bg-equip2 z-8 absolute flex flex-col top-[50%] left-[50%] translate-x-[-46%] translate-y-[-48%]">
+          <div onClick={blockPropagation} className="bg-equip2 z-0 absolute flex flex-col top-[50%] left-[50%] translate-x-[-46%] translate-y-[-48%]">
             <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible" />
             <div className="content absolute inset-0 p-24 flex flex-col">
               {/* Content goes here */}
