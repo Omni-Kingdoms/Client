710985e7ac83986cd1eb3c67c6197e4aa88ab1eb
caiolima-stage <caio.lima@stage.consulting>
2023-08-14 17:39:20 -0300

    feat: both modals layer and positioning strategy

--- src/components/Modal/Equipment/Equipment.tsx
+++ src/components/Modal/Equipment/Equipment.tsx
@@ -6,6 +6,7 @@ import { useOnClickOutside } from 'usehooks-ts';
 
 import fechar from "@/assets/img/components/modal/X.png";
 import paperback1 from '@/assets/img/components/Equipment/paperback1.png';
+import paperback2 from '@/assets/img/components/Equipment/paperback2.png';
 
 type EquipmentProps = {
   close: () => void
@@ -18,14 +19,22 @@ export default function Equipment({ close }: EquipmentProps) {
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
-        <div ref={equipmentRef} className="bg-equip relative flex flex-col">
-          <Image src={paperback1} width={1000} alt="Equipment background" className="invisible" />
-          <div className="content absolute inset-0 p-24 flex flex-col">
-            <button type="button" className="absolute top-22 right-16" onClick={close}>
-              <Image src={fechar} alt="Fechar lista" />
-            </button>
-            {/* Content goes here */}
+      <div className="fixed inset-0 backdrop-blur-sm">
+        <div className="relative h-[100vh]">
+          <button type="button" className="absolute top-22 right-16" onClick={close}>
+            <Image src={fechar} alt="Fechar lista" />
+          </button>
+          <div className="bg-equip z-10 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+            <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible" />
+            <div className="content absolute inset-0 p-24 flex flex-col">
+              {/* Content goes here */}
+            </div>
+          </div>
+          <div className="bg-equip2 z-8 absolute flex flex-col top-[50%] left-[50%] translate-x-[-46%] translate-y-[-48%]">
+            <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible" />
+            <div className="content absolute inset-0 p-24 flex flex-col">
+              {/* Content goes here */}
+            </div>
           </div>
         </div>
       </div>
