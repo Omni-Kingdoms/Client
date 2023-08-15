948c2e0b8d5a0b5ece2d37741814264091be0cff
caiolima-stage <caio.lima@stage.consulting>
2023-08-15 18:04:06 -0300

    feat: slot positioning strategy

--- src/components/Modal/Equipment/ItemSlots.tsx
+++ src/components/Modal/Equipment/ItemSlots.tsx
@@ -1,5 +1,6 @@
 import getClassBackground from '@/components/utils/getClassBackground';
 import { playerStore } from '@/store/playerStore'
+import Slot from './Slot';
 
 export default function ItemSlots() {
   const player = playerStore((state) => state.currentPlayer);
@@ -11,8 +12,14 @@ export default function ItemSlots() {
   const classBackground = getClassBackground(Number(player.playerClass), player.male);
 
   return (
-    <div className={`h-[100%] w-[100%] persona-bg ${classBackground}`}>
-
+    <div className={`h-[100%] w-[100%] base-bg ${classBackground} relative`}>
+      <Slot bg={2} className="w-[18%] absolute top-[6%] left-[15%]" />
+      <Slot bg={1} className="w-[18%] absolute top-[36%] left-[2%]" />
+      <Slot bg={2} className="w-[12%] absolute top-[72%] left-[14%]" />
+      <Slot bg={2} className="w-[18%] absolute top-[5%] right-[10%]" />
+      <Slot bg={1} className="w-[10%] absolute top-[38%] right-[18%]" />
+      <Slot bg={1} className="w-[10%] absolute top-[50%] right-[6%]" />
+      <Slot bg={2} className="w-[12%] absolute top-[75%] right-[10%]" />
     </div>
   )
 }
---
+++ src/components/Modal/Equipment/Slot.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import slotBackground1 from '../../../assets/img/components/Equipment/itemMask1.png';
+import slotBackground2 from '../../../assets/img/components/Equipment/itemMask2.png';
+
+type SlotProps = {
+  className?: string,
+  bg: 1 | 2
+}
+
+function Slot({ className, bg }: SlotProps) {
+  return (
+    <div className={`base-bg inline-block slot-bg-${bg}${className ? ` ${className}` : ''}`}>
+      <Image
+        src={bg === 1 ? slotBackground1 : slotBackground2}
+        alt="Slot background"
+        className="invisible w-[100%]"
+      />
+    </div>
+  )
+}
+
+export default Slot
