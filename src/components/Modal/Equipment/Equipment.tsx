4f2c132e2faef3636595fd7adcc593a95c24ced8
caiolima-stage <caio.lima@stage.consulting>
2023-08-16 17:25:28 -0300

    fix: close button positioning

--- src/components/Modal/Equipment/Equipment.tsx
+++ src/components/Modal/Equipment/Equipment.tsx
@@ -26,13 +26,8 @@ export default function Equipment({ close }: EquipmentProps) {
         <div className="relative h-[100vh]">
           <div onClick={blockPropagation} className="bg-equip z-20 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] pointer-events-none">
             <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible max-w-[80vw]" />
-            <div className="content absolute inset-0 p-24 flex flex-col">
-            <button type="button" className="absolute top-18 right-0 z-20" onClick={close}>
-              <Image src={closeIcon} alt="close icon" />
-            </button>
             <div className="content absolute inset-0 p-24">
               <ItemSlots />
-            </div>
             </div>
           </div>
           <div
@@ -45,6 +40,9 @@ export default function Equipment({ close }: EquipmentProps) {
           >
             <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible max-w-[80vw]" />
             <div className="content absolute inset-0 p-24 flex justify-end">
+            <button type="button" className="absolute top-12 right-14 z-20" onClick={close}>
+              <Image src={closeIcon} alt="close icon" />
+            </button>
               <PlayerStats
                 open={() => setIsSubmodalOpen(true)}
                 close={() => setIsSubmodalOpen(false)}
