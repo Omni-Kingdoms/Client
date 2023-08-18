8aabe3ad992e4a6c558ee71d2b32d74bca869724
caiolima-stage <caio.lima@stage.consulting>
2023-08-18 08:41:26 -0300

    feat: win and loss stat counter

--- src/components/Modal/Equipment/PlayerStats.tsx
+++ src/components/Modal/Equipment/PlayerStats.tsx
@@ -1,6 +1,8 @@
 import arrowLeft from "@/assets/img/components/PlayerCard/icons/arrow-left.svg"
 import arrowRight from "@/assets/img/components/PlayerCard/icons/arrow-right.svg"
 import PlayerCharacterInfo from '@/components/PlayerCard/PlayerCharacterInfo'
+import lossImage from '@/assets/img/components/Equipment/loss.png'
+import winImage from '@/assets/img/components/Equipment/win.png'
 import Image from 'next/image'
 
 type PlayerStatsProps = {
@@ -12,8 +14,19 @@ type PlayerStatsProps = {
 function PlayerStats({ open, close, isOpen }: PlayerStatsProps) {
   return (
     <div className="flex flex-col">
-      <div className="content flex-1 relative pr-5 -mt-8">
+      <div className="content flex-1 relative -mt-8 flex flex-col ">
         <PlayerCharacterInfo small />
+        <div className="stat-table flex-1"></div>
+        <div className="flex -mr-5">
+          <div className="stat-counter flex relative items-center -mr-6">
+            <Image src={winImage} width={100} alt="win quantifier" />
+            <p className="text-4xl translate-x-[-60%] text-[#7BAA74]">00</p>
+          </div>
+          <div className="stat-counter flex items-center">
+            <Image src={lossImage} width={100} alt="loss quantifier" />
+            <p className="text-4xl translate-x-[-30%] translate-y-[-10%] text-[#8C3A20]">00</p>
+          </div>
+        </div>
       </div>
       <div className="actions flex justify-end gap-5 -mr-2">
         <button type="button" onClick={close}>
