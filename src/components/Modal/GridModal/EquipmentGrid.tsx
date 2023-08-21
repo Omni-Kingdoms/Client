75934c99ffb75a732b87d6b9f7db9648a34ba65c
caiolima-stage <caio.lima@stage.consulting>
2023-08-21 17:35:58 -0300

    feat: grid modal pagination

--- src/components/Modal/GridModal/EquipmentGrid.tsx
+++ src/components/Modal/GridModal/EquipmentGrid.tsx
@@ -4,6 +4,7 @@ import GridItemBox from './GridItemBox';
 import arrowLeft from "@/assets/img/components/PlayerCard/icons/arrow-left.svg"
 import arrowRight from "@/assets/img/components/PlayerCard/icons/arrow-right.svg"
 import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
+import { useMemo, useState } from 'react';
 
 type EquipmentGridProps = {
   playerEquipments: Equip[],
@@ -12,34 +13,70 @@ type EquipmentGridProps = {
 }
 
 export default function EquipmentGrid({ playerEquipments, setCurrentEquipment, title }: EquipmentGridProps) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const amountOfPages = useMemo(() => Math.ceil(playerEquipments.length /24), [playerEquipments.length]);
+
+  console.log(playerEquipments);
+
+  function handlePageForwards() {
+    if (currentPage >= amountOfPages) return;
+
+    setCurrentPage((prev) => prev + 1);
+  }
+
+  function handlePageBackwards() {
+    if (currentPage <= 1) return;
+
+    setCurrentPage((prev) => prev - 1);
+  }
+
+  const playerEquipmentsToBeShown = useMemo(() => (
+    playerEquipments.slice(currentPage === 1 ? ((currentPage - 1) * 24) : ((currentPage - 1) * 24 - 1), 24)
+  ), [currentPage, playerEquipments]);
+
+  console.log(playerEquipmentsToBeShown);
+
   return (
     <div className="equipment-grid-container flex flex-col">
       <h1 className="title text-4xl mb-4">{title}</h1>
       <div className="flex-1 grid grid-cols-6 gap-4">
-        <button className="title choosen-button">Head</button>
-        <button className="title">Neck</button>
-        <button className="title">Body</button>
-        <button className="title">Hands</button>
-        <button className="title">Pants</button>
-        <button className="title">Feet</button>
+        <button className="title choosen-button invisible">Head</button>
+        <button className="title invisible">Neck</button>
+        <button className="title invisible">Body</button>
+        <button className="title invisible">Hands</button>
+        <button className="title invisible">Pants</button>
+        <button className="title invisible">Feet</button>
         <div className="col-span-6 grid grid-rows-4 grid-cols-6 gap-4">
           {
             Array.from({ length: 24 }, (_, i) => i + 1).map((i) => (
-              <GridItemBox key={i} item={playerEquipments[i - 1]} setCurrentEquipment={setCurrentEquipment} />
+              <GridItemBox key={i} item={playerEquipmentsToBeShown[i - 1]} setCurrentEquipment={setCurrentEquipment} />
             ))
           }
         </div>
       </div>
       <div className="h-32 flex items-center justify-center gap-8">
-        <button type="button" className="gray-icon" disabled>
+        <button
+          type="button"
+          className={currentPage <= 1 ? 'gray-icon' : ''}
+          onClick={handlePageBackwards}
+          disabled={currentPage <= 1}
+        >
           <Image src={arrowLeft} width={40} alt="backwards page" />
         </button>
         <div className="pages flex items-center gap-3">
-          <p className="title text-2xl choosen-text">1</p>
-          <p className="title text-2xl">2</p>
-          <p className="title text-2xl">3</p>
+          {
+            Array.from({ length: amountOfPages }, (_, i) => i + 1).map((i) => (
+              <p key={i} className={`title text-2xl ${currentPage === i ? 'choosen-text' : ''}`}>{i}</p>
+            ))
+          }
         </div>
-        <button type="button">
+        <button
+          type="button"
+          className={currentPage >= amountOfPages ? 'gray-icon' : ''}
+          onClick={handlePageForwards}
+          disabled={currentPage >= amountOfPages}
+        >
           <Image src={arrowRight} width={40} alt="forwards page" />
         </button>
       </div>
