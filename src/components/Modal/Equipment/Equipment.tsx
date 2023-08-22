5cb33ee4b58c2e9c1213297ddbc4fb91bdb6e6db
Alexandre Carvalheira <axxcar@gmail.com>
2023-08-22 18:03:43 -0300

    fix equipment array

--- src/components/Modal/Equipment/Equipment.tsx
+++ src/components/Modal/Equipment/Equipment.tsx
@@ -1,24 +1,24 @@
 import "./index.css";
 
-import { MouseEvent, useCallback, useEffect, useState } from 'react'
-import Image from 'next/image';
+import { MouseEvent, useCallback, useEffect, useState } from "react";
+import Image from "next/image";
 
 import closeIcon from "@/assets/img/components/modal/X.png";
-import paperback1 from '@/assets/img/components/Equipment/paperback1.png';
-import paperback2 from '@/assets/img/components/Equipment/paperback2.png';
-import { playerStore } from '@/store/playerStore';
-import { contractStore } from '@/store/contractStore';
-import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
-import Loading from '@/app/play/loading';
-import ItemSlots from './components/ItemSlots';
-import PlayerStats from './components/PlayerStats';
-import EquipmentList from '../GridModal/EquipmentList';
-import { toast } from 'react-toastify';
-import { usePublicClient } from 'wagmi';
+import paperback1 from "@/assets/img/components/Equipment/paperback1.png";
+import paperback2 from "@/assets/img/components/Equipment/paperback2.png";
+import { playerStore } from "@/store/playerStore";
+import { contractStore } from "@/store/contractStore";
+import { BasicEquipmentStruct as Equip } from "@/types/DIAMOND1HARDHAT";
+import Loading from "@/app/play/loading";
+import ItemSlots from "./components/ItemSlots";
+import PlayerStats from "./components/PlayerStats";
+import EquipmentList from "../GridModal/EquipmentList";
+import { toast } from "react-toastify";
+import { usePublicClient } from "wagmi";
 
 type EquipmentProps = {
-  close: () => void
-}
+  close: () => void;
+};
 
 export default function Equipment({ close }: EquipmentProps) {
   const contract = contractStore((state) => state.diamond);
@@ -28,7 +28,8 @@ export default function Equipment({ close }: EquipmentProps) {
   const currentPlayerIndex = playerStore((state) => state.currentPlayerIndex);
 
   const [isSubmodalOpen, setIsSubmodalOpen] = useState<boolean>(false);
-  const [isEquipmentListOpen, setIsEquipmentListOpen] = useState<boolean>(false);
+  const [isEquipmentListOpen, setIsEquipmentListOpen] =
+    useState<boolean>(false);
   const [userEquipments, setUserEquipments] = useState<Equip[]>();
   const [isLoading, setIsLoading] = useState(true);
 
@@ -41,7 +42,9 @@ export default function Equipment({ close }: EquipmentProps) {
   const gatherEquippedItems = useCallback(async () => {
     try {
       const equipmentIds = Object.values(currentPlayer?.slot!);
-      const promises = equipmentIds.map((equipmentId) => contract.read.getEquipment([equipmentId]));
+      const promises = equipmentIds.map((equipmentId) =>
+        contract.read.getEquipment([equipmentId])
+      );
 
       let equipments: Equip[] = await Promise.all(promises);
 
@@ -57,15 +60,13 @@ export default function Equipment({ close }: EquipmentProps) {
 
   const handleGatherPlayerEquipmentInformation = useCallback(async () => {
     try {
-      const equipments: number[] = (await contract.read.getPlayerToEquipment([
-        players[currentPlayerIndex],
-      ])).map((equipItem: BigInt) => Number(equipItem));
-
+      const equipments: number[] = (
+        await contract.read.getPlayerToEquipment([players[currentPlayerIndex]])
+      ).map((equipItem: BigInt) => Number(equipItem));
       let equipmentList: Equip[] = [];
 
-      for (let i = 1; i <= equipments.length; i++) {
-        const equipment = await contract.read.getEquipment([i]);
-
+      for (let i = 0; i < equipments.length; i++) {
+        const equipment = await contract.read.getEquipment([equipments[i]]);
         equipmentList.push(equipment);
       }
 
@@ -75,13 +76,21 @@ export default function Equipment({ close }: EquipmentProps) {
     }
   }, [contract.read, currentPlayerIndex, players]);
 
-  const isEquipmentEquipped = useCallback((currentEquipment: Equip) => (
-    Boolean(Object.values(currentPlayer?.slot!).find((slot) => slot == currentEquipment?.id))
-  ), [currentPlayer?.slot]);
+  const isEquipmentEquipped = useCallback(
+    (currentEquipment: Equip) =>
+      Boolean(
+        Object.values(currentPlayer?.slot!).find(
+          (slot) => slot == currentEquipment?.id
+        )
+      ),
+    [currentPlayer?.slot]
+  );
 
   async function handleEquip(currentEquipment: Equip) {
     try {
-      const method = isEquipmentEquipped(currentEquipment) ? contract.write.unequip : contract.write.equip;
+      const method = isEquipmentEquipped(currentEquipment)
+        ? contract.write.unequip
+        : contract.write.equip;
 
       console.log(method);
 
@@ -133,75 +142,86 @@ export default function Equipment({ close }: EquipmentProps) {
   }
 
   useEffect(() => {
-    gatherEquippedItems()
+    gatherEquippedItems();
   }, [gatherEquippedItems]);
 
   return (
     <>
-      <div className={`fixed z-10 inset-0 overflow-y-auto ${isEquipmentListOpen ? 'hidden' : ''}`}>
+      <div
+        className={`fixed z-10 inset-0 overflow-y-auto ${
+          isEquipmentListOpen ? "hidden" : ""
+        }`}
+      >
         <div onClick={close} className="fixed inset-0 backdrop-blur-sm">
           <div className="relative h-[100vh]">
-            <div onClick={blockPropagation} className="bg-equip z-20 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] pointer-events-none">
-              <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible max-w-[95vw]" />
+            <div
+              onClick={blockPropagation}
+              className="bg-equip z-20 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] pointer-events-none"
+            >
+              <Image
+                src={paperback1}
+                width={1000}
+                alt="Equipment1 background"
+                className="invisible max-w-[95vw]"
+              />
               <div className="content absolute inset-0 p-16 sm:p-20 md:p-24">
-                {
-                  isLoading || !userEquipments
-                    ? (
-                      <div className="w-[100%] h-[100%] flex items-center justify-center">
-                        <Loading />
-                      </div>
-                    )
-                    : (
-                      <ItemSlots
-                        openEquipmentList={() => setIsEquipmentListOpen(true)}
-                        userEquipments={userEquipments}
-                      />
-                    )
-                }
+                {isLoading || !userEquipments ? (
+                  <div className="w-[100%] h-[100%] flex items-center justify-center">
+                    <Loading />
+                  </div>
+                ) : (
+                  <ItemSlots
+                    openEquipmentList={() => setIsEquipmentListOpen(true)}
+                    userEquipments={userEquipments}
+                  />
+                )}
               </div>
             </div>
             <div
               onClick={blockPropagation}
-              className={
-                `animate-transform bg-equip2 z-0 absolute flex
+              className={`animate-transform bg-equip2 z-0 absolute flex
                 flex-col top-[50%] left-[50%] translate-x-[-42%]
-                translate-y-[-48%]${isSubmodalOpen ? ' isOpen' : ''}`
-              }
+                translate-y-[-48%]${isSubmodalOpen ? " isOpen" : ""}`}
             >
-              <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible max-w-[95vw]" />
+              <Image
+                src={paperback2}
+                width={950}
+                alt="Equipment2 background"
+                className="invisible max-w-[95vw]"
+              />
               <div className="content absolute inset-0 p-16 sm:p-20 md:p-24 flex justify-end">
-              <button type="button" className="absolute top-12 right-14 z-20" onClick={close}>
-                <Image src={closeIcon} alt="close icon" />
-              </button>
-                {
-                  !isLoading && userEquipments && (
-                    <PlayerStats
-                      open={() => setIsSubmodalOpen(true)}
-                      close={() => setIsSubmodalOpen(false)}
-                      userEquipments={userEquipments}
-                      isOpen={isSubmodalOpen}
-                    />
-                  )
-                }
+                <button
+                  type="button"
+                  className="absolute top-12 right-14 z-20"
+                  onClick={close}
+                >
+                  <Image src={closeIcon} alt="close icon" />
+                </button>
+                {!isLoading && userEquipments && (
+                  <PlayerStats
+                    open={() => setIsSubmodalOpen(true)}
+                    close={() => setIsSubmodalOpen(false)}
+                    userEquipments={userEquipments}
+                    isOpen={isSubmodalOpen}
+                  />
+                )}
               </div>
             </div>
           </div>
         </div>
       </div>
-      {
-        isEquipmentListOpen && (
-          <EquipmentList
-            back={() => setIsEquipmentListOpen(false)}
-            close={close}
-            handleGatherEquipInfo={handleGatherPlayerEquipmentInformation}
-            title="Equipment"
-            buttonText="Equip"
-            altButtonText="Unequip"
-            action={handleEquip}
-            type="equipment"
-          />
-        )
-      }
+      {isEquipmentListOpen && (
+        <EquipmentList
+          back={() => setIsEquipmentListOpen(false)}
+          close={close}
+          handleGatherEquipInfo={handleGatherPlayerEquipmentInformation}
+          title="Equipment"
+          buttonText="Equip"
+          altButtonText="Unequip"
+          action={handleEquip}
+          type="equipment"
+        />
+      )}
     </>
-  )
+  );
 }
