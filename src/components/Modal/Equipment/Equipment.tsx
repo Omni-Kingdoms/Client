9f3fdd4bd6bc7badd64f5e30d217fa04b4a5c1ac
caiolima-stage <caio.lima@stage.consulting>
2023-08-22 16:32:38 -0300

    fix: equip class image mask

--- src/components/Modal/Equipment/Equipment.tsx
+++ src/components/Modal/Equipment/Equipment.tsx
@@ -142,7 +142,7 @@ export default function Equipment({ close }: EquipmentProps) {
         <div onClick={close} className="fixed inset-0 backdrop-blur-sm">
           <div className="relative h-[100vh]">
             <div onClick={blockPropagation} className="bg-equip z-20 absolute flex flex-col top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] pointer-events-none">
-              <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible max-w-[85vw]" />
+              <Image src={paperback1} width={1000} alt="Equipment1 background" className="invisible max-w-[95vw]" />
               <div className="content absolute inset-0 p-24">
                 {
                   isLoading || !userEquipments
@@ -168,7 +168,7 @@ export default function Equipment({ close }: EquipmentProps) {
                 translate-y-[-48%]${isSubmodalOpen ? ' isOpen' : ''}`
               }
             >
-              <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible max-w-[85vw]" />
+              <Image src={paperback2} width={950} alt="Equipment2 background" className="invisible max-w-[95vw]" />
               <div className="content absolute inset-0 p-24 flex justify-end">
               <button type="button" className="absolute top-12 right-14 z-20" onClick={close}>
                 <Image src={closeIcon} alt="close icon" />
