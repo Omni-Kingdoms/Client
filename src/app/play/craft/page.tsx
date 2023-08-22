865045ed87ad3028b12f275896e5fe371c2c3bfd
caiolima-stage <caio.lima@stage.consulting>
2023-08-22 09:06:21 -0300

    feat: craft modal refactored

--- src/app/play/craft/page.tsx
+++ src/app/play/craft/page.tsx
@@ -1,44 +1,14 @@
 "use client";
 import Image from "next/image";
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { Tooltip } from 'antd';
 import map from "@/assets/img/components/Craft/map.png";
 import craft from "@/assets/img/components/Play/craft.png";
 import EquipmentList from '@/components/Modal/GridModal/EquipmentList';
-import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
-import { contractStore } from '@/store/contractStore';
-import { playerStore } from '@/store/playerStore';
+import CraftModal from '@/components/Modal/Craft/CraftModal';
 
 export default function Craft() {
-  const contract = contractStore((state) => state.diamond);
-  const players = playerStore((state) => state.players);
-  const currentPlayerIndex = playerStore((state) => state.currentPlayerIndex);
-
   const [isBasicCraftModalOpen, setIsBasicCraftModalOpen] = useState(false);
-
-  const handleGatherPlayerEquipmentInformation = useCallback(async () => {
-    try {
-      const equipments: number[] = (await contract.read.getPlayerToEquipment([
-        players[currentPlayerIndex],
-      ])).map((equipItem: BigInt) => Number(equipItem));
-
-      let equipmentList: Equip[] = [];
-
-      for (let i = 1; i <= equipments.length; i++) {
-        const equipment = await contract.read.getEquipment([i]);
-
-        equipmentList.push(equipment);
-      }
-
-      return equipmentList;
-    } catch (err) {
-      console.log(err);
-    }
-  }, [contract.read, currentPlayerIndex, players]);
-
-  async function handleBasicCraftUpgrade(currentEquipment: Equip) {
-    console.log('basic craft: ', currentEquipment);
-  }
 
   return(
     <>
@@ -60,13 +30,8 @@ export default function Craft() {
       </div>
       {
         isBasicCraftModalOpen && (
-          <EquipmentList
-            handleGatherEquipInfo={handleGatherPlayerEquipmentInformation}
+          <CraftModal
             close={() => setIsBasicCraftModalOpen(false)}
-            title="Craft"
-            buttonText="Upgrade"
-            action={handleBasicCraftUpgrade}
-
           />
         )
       }
---
+++ src/components/Modal/Craft/CraftModal.tsx
@@ -0,0 +1,49 @@
+import { useCallback } from 'react';
+import EquipmentList from '../GridModal/EquipmentList';
+import { playerStore } from '@/store/playerStore';
+import { contractStore } from '@/store/contractStore';
+import { BasicEquipmentStruct as Equip } from '@/types/DIAMOND1HARDHAT';
+
+type CraftModalProps = {
+  close: () => void
+}
+
+export default function CraftModal({ close }: CraftModalProps) {
+  const contract = contractStore((state) => state.diamond);
+  const players = playerStore((state) => state.players);
+  const currentPlayerIndex = playerStore((state) => state.currentPlayerIndex);
+
+  const handleGatherPlayerEquipmentInformation = useCallback(async () => {
+    try {
+      const equipments: number[] = (await contract.read.getPlayerToEquipment([
+        players[currentPlayerIndex],
+      ])).map((equipItem: BigInt) => Number(equipItem));
+
+      let equipmentList: Equip[] = [];
+
+      for (let i = 1; i <= equipments.length; i++) {
+        const equipment = await contract.read.getEquipment([i]);
+
+        equipmentList.push(equipment);
+      }
+
+      return equipmentList;
+    } catch (err) {
+      console.log(err);
+    }
+  }, [contract.read, currentPlayerIndex, players]);
+
+  async function handleBasicCraftUpgrade(currentEquipment: Equip) {
+    console.log('basic craft: ', currentEquipment);
+  }
+
+  return (
+    <EquipmentList
+      handleGatherEquipInfo={handleGatherPlayerEquipmentInformation}
+      close={close}
+      title="Craft"
+      buttonText="Upgrade"
+      action={handleBasicCraftUpgrade}
+    />
+  )
+}
