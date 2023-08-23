9ac31a7167aee0e0d5a1c8b984cbc56c6267d1e8
caiolima-stage <caio.lima@stage.consulting>
2023-08-23 18:33:34 -0300

    feat: switch network and change name icons on utility screen

--- src/app/play/utility/page.tsx
+++ src/app/play/utility/page.tsx
@@ -1,8 +1,10 @@
-/* import Image from 'next/image';
-
-import "./index.css";
+/* "use client";
+import Image from 'next/image';
 
 import map from '@/assets/img/components/Craft/map.png';
+import switchNetworkIcon from '@/assets/img/components/Utility/switch-network.png';
+import changeNameIcon from '@/assets/img/components/Utility/change-name.png';
+import { Tooltip } from 'antd';
 
 export default function Utility() {
   return (
@@ -11,6 +13,24 @@ export default function Utility() {
         <Image src={map} alt="Mapa" className="invisible" />
       </div>
       <div className="icon-right min-[2000px]:right-64 min-[3000px]:mr-96">
+        <Tooltip title="Change name">
+          <button>
+            <Image
+              src={changeNameIcon}
+              className="icons-map hover:cursor-pointer icons-map min-[400px]:m-5"
+              alt="change name icon"
+            />
+          </button>
+        </Tooltip>
+        <Tooltip title="Switch network">
+          <button>
+            <Image
+              src={switchNetworkIcon}
+              className="icons-map hover:cursor-pointer icons-map min-[400px]:m-5"
+              alt="mapa"
+            />
+          </button>
+        </Tooltip>
       </div>
     </div>
   )
