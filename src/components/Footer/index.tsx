e1c279a5ea4cb0cb351b7a681d8bc21cdacf04fa
caiolima-stage <caio.lima@stage.consulting>
2023-09-01 19:03:57 -0300

    fix: notifier popup layer issue

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
           <NotifierPopup />
         )
       }
-      <footer className="flex justify-center gap-5 p-4 z-30">
+      <footer className="flex justify-center gap-5 p-4 z-50">
         <div className="items-center text-footer">
           <p> OmniKingdoms Copyright © 2023 - All right reserved</p>
         </div>
